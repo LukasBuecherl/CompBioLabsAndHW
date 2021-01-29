20fc0c5133855b7567fbe1ba7cdaacfab59646d0
LukasBuecherl <lukas.buecherl@colorado.edu>
2021-01-29 11:05:38 -0700

    Deleted old files

--- Labs/Lab01/Lab1.sh
+++
@@ -1,8 +0,0 @@
-grep Swallow BirdList.txt | grep -v Swallow-tail | tail +2
-grep Swallow BirdList.txt | grep -v Swallow-tail | tail +2 | wc -l
-
-cut -f 2-4 -d , PredPreyData.csv > PredPreyData_cut.csv 
-cut -f 2-4 -d , PredPreyData.csv | tail 
-
-cut -f 2-4 -d , PredPreyData.csv | head -n 1 > PredPreyData_modified.csv
-cut -f 2-4 -d , PredPreyData.csv | tail >> PredPreyData_modified.csv 
