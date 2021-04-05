3d5f3645f35cb54de62d69725b7de0bf4bc74ead
LukasBuecherl <lukas.buecherl@colorado.edu>
2021-04-05 17:52:51 -0600

    Update README

---
+++ Final_Project/Buecherl_FinalProject.R
@@ -0,0 +1,9 @@
+# EBIO 5420: Computational Biology 
+# Prof: Samuel Flaxman
+# Student: Lukas Buecherl
+# Final Project
+###############################################################################
+
+
+WoodData <- read.csv("/Users/lukas/Library/Mobile Documents/com~apple~CloudDocs/CU Boulder/Spring 2021/Computational Biology/CompBioLabsAndHW/Final_Project/sample_tree_growth_data_with_locations.csv", stringsAsFactors = F)
+which(is.na(WoodData))
